2071aecd6c3a1880958be997ffddbeb713a5d3a5
Clément Beffa <clement@beffa.org>
2016-02-16 20:58:19 +0100

    Badge icon support

--- gInbox/AppDelegate.swift
+++ gInbox/AppDelegate.swift
@@ -11,9 +11,13 @@ import Cocoa
 import WebKit
 import AppKit
 import ApplicationServices
+import Alamofire
+import SWXMLHash
+
 
 class AppDelegate : NSObject, NSApplicationDelegate {
 	var mailto : NSURL?
+	var timer : NSTimer!
 
 	@IBOutlet var window: NSWindow!
 	@IBOutlet weak var webViewController: WebViewController?
@@ -23,10 +27,14 @@ class AppDelegate : NSObject, NSApplicationDelegate {
 		super.init()
 		let appleEventManager = NSAppleEventManager.sharedAppleEventManager()
 		appleEventManager.setEventHandler(self, andSelector: Selector("handleGetURLEvent:replyEvent:"), forEventClass: UInt32(kInternetEventClass), andEventID: UInt32(kAEGetURL))
+		timer = NSTimer.scheduledTimerWithTimeInterval(60, target: self, selector: Selector("updateBadge"), userInfo: nil, repeats: false)
 	}
 
     func applicationDidFinishLaunching(notification: NSNotification) {
         self.window.collectionBehavior = NSWindowCollectionBehavior.FullScreenPrimary
+		updateBadge()
+	}
+	
 	func handleGetURLEvent(event : NSAppleEventDescriptor,  replyEvent: NSAppleEventDescriptor)
 	{
 		if let urlString = event.paramDescriptorForKeyword(UInt32(keyDirectObject))?.stringValue {
@@ -46,4 +54,16 @@ class AppDelegate : NSObject, NSApplicationDelegate {
         }
         return true
     }
+	func updateBadge() {
+		Alamofire.request(.GET, "https://mail.google.com/mail/u/0/feed/atom", parameters: nil)
+			.response { (request, response, data, error) in
+				let xml = SWXMLHash.parse(data!)
+				if let unread = xml["feed"]["fullcount"].element?.text where Int(unread) > 0 {
+					NSApplication.sharedApplication().dockTile.badgeLabel = unread
+				} else {
+					NSApplication.sharedApplication().dockTile.badgeLabel = nil
+				}
+		}
+	}
+	
 }
